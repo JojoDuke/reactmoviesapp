aba35c66eff986aded25ee994b05639d5ee4c255
JojoDuke <jojoamankwa@gmail.com>
2021-09-21 20:56:37 +0000

    Added simple movies list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css"
+import MoviesList from "./components/MoviesList";
+import ListHeading from './components/ListHeading';
+import { SearchBox } from './components/SearchBox';
 
 const App = () => {
-  const [movies, setMovies] = useState([])
-  
-  return <div>Hello!</div>
-}
+  //Set state
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+
+  // Request movies from API
+  const getMovieRequest = async () => {
+    const url = "http://www.omdbapi.com/?s=avengers&apikey=ce990560";
+    const response = await fetch(url);
+    const responseJson = await response.json();
+
+    setMovies(responseJson.Search);
+  };
+
+  useEffect(() => {
+    getMovieRequest();
+  }, []);
+
+  return (
+    <div className="container-fluid">
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <ListHeading heading="Movies" />
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
+      </div>
+
+      <div className="row movie-app">
+        <MoviesList movies={movies}/>
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -4,12 +4,12 @@ const MoviesList = (props) => {
     return (
         <div>
             {props.movies.map((movie, index) => (
-                <div>
-                    <img src="{ movie.Poster }" alt="movie" />
+                <div className="d-flex justify-content-start m-4">
+                    <img src={movie.Poster} alt="movie" />
                 </div>
             ))}
         </div>
     )
 }
 
-export default MoviesList
+export default MoviesList;
