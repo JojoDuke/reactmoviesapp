d1f458311366a3a6a57225e166747737769a4822
JojoDuke <jojoamankwa@gmail.com>
2021-09-23 23:31:10 +0000

    Getting somewhere

--- src/components/MainMovie.js
+++ src/components/MainMovie.js
@@ -1,20 +1,28 @@
+/* eslint-disable react/style-prop-object */
 import React from 'react'
 
-const MainMovie = () => {
+const MainMovie = (props) => {
     return (
-        <div>
-            <img src="" alt="main_movie" />
-            <div>
-                <p>Name</p>
-                <p>Description</p>
-                <p>Actors</p>
+        <>
+            {props.movies.map((movie, index) => (
+                // eslint-disable-next-line react/style-prop-object
+            <div className="bg-hero">
+                    <img className="bg-image" src={movie.Poster} alt='movie'></img>
 
-                <div>
-                    <button>Watch Now</button>
-                    <button>+</button>
+                <div className="m-xl-5 w-50 position-absolute text-white">
+                    <h2 className="fw-bolder">{movie.Title}</h2>
+                    <p>{movie.Plot}</p>
+                    <p>{movie.Actors}</p>
+
+                    {/* Watch Buttons */}
+                    <div>
+                        <button className="btn btn-lg watch-now">Watch Now</button>
+                        <button className="btn btn-lg add-favourite">+</button>
+                    </div>
                 </div>
             </div>
-        </div>
+            ))}
+        </>
     )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import '../App.css'
 const Navbar = (props) => {
     return (
         <div>
-            <nav className="navbar shadow px-3 py-4">
+            <nav className="navbar shadow-lg px-3 py-4">
                 <div class="container-fluid">
                     <span class="navbar-brand mb-0 h1 text-black">CINETIME</span>
                     <SearchBox/>
