bb7f34af9cd99a9e37486056b1243273f033bd74
JojoDuke <jojoamankwa@gmail.com>
2021-09-25 15:02:47 +0000

    This hard bro

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,19 +1,13 @@
 import React from 'react';
 
 const MoviesList = (props) => {
-	const FavouriteComponent = props.favouriteComponent;
+	//const FavouriteComponent = props.favouriteComponent;
 
 	return (
 		<>
 			{props.movies.map((movie, index) => (
-				<div className='image-container d-flex justify-content-start m-3'>
+				<div className=''>
 					<img src={movie.Poster} alt='movie'></img>
-					<div
-						onClick={() => props.handleFavouritesClick(movie)}
-						className='overlay d-flex align-items-center justify-content-center'
-					>
-						<FavouriteComponent />
-					</div>
 				</div>
 			))}
 		</>
