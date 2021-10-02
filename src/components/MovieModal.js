f92b2d953df3f52a8ff2ca1a230aaf0ee30f2b08
JojoDuke <jojoamankwa@gmail.com>
2021-10-02 16:07:27 +0000

    Added Movie Modal

--- src/components/MovieModal.js
+++ src/components/MovieModal.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
 export const MovieModal = () => {
     return (
-        <div>
-            
+        <div className="movie-modal shadow-lg">
+            <div>Video</div>
+            <div>Description</div>
+            <div className="d-flex flex-column">
+                <button className="btn btn-primary">Watch Now</button>
+                <button className="btn btn-primary btn-fav">Add to Favourites <FontAwesomeIcon color="red" icon={faHeart}/> </button>
+            </div>
         </div>
     )
 }
